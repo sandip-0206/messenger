e612795c7f83be76e1ba2ef869f6942acbfc2eaa
Sandip Sharma <112794092+sandip-0206@users.noreply.github.com>
2023-12-30 19:13:27 +0530

    Complete Auth UI

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image"
 
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
@@ -21,7 +22,7 @@ export default function Home() {
          Sign in to your account
         </h2>
       </div>
-      {/* AuthForm */}
+      <AuthForm />
     </div>
   )
 }
