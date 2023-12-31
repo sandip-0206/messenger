922b5055839856b571710aa7da316a8be06cf747
Sandip Sharma <112794092+sandip-0206@users.noreply.github.com>
2023-12-31 15:21:39 +0530

    Complete Login Functionality and Social Login (Google and Github)

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Button from "@/app/components/Button";
-import Input from "@/app/components/inputs/input";
+import Input from "../../components/inputs/input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import {BsGithub, BsGoogle } from 'react-icons/bs'
 
 import axios from "axios";
+import {toast} from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -41,10 +43,26 @@ const AuthForm = () => {
         if(variant === 'REGISTER'){
             //Axios Register
             axios.post('/api/register', data)
+            .catch(() => toast.error('Something went wrong!'))
+            .finally(() => setIsLoading(false))
         }
 
         if(variant === 'LOGIN'){
             //NextAuth SignIn
+            signIn('credentials', {
+                ...data,
+                redirect: false
+            })
+            .then((callback) => {
+                if(callback?.error){
+                    toast.error('Invalid Credentials')
+                }
+
+                if(callback?.ok && !callback?.error){
+                    toast.success('Loged in!')
+                }
+            })
+            .finally(() => setIsLoading(false))
         }
     }
 
@@ -52,6 +70,16 @@ const AuthForm = () => {
         setIsLoading(true)
 
         //NextAuth Social Sign In
+        signIn(action, { redirect: false })
+        .then((callback) => {
+            if(callback?.error){
+                toast.error('Invalid Credentials!')
+            }
+            if(callback?.ok && !callback?.error){
+                toast.success('Logged in!')
+            }
+        })
+        .finally(() => setIsLoading(false))
     }
 
     return ( 
